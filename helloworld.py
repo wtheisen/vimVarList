ef20c0919c72fb47d23a50bf644a9a6570d4a1c4
Bill Theisen <bilbo991@wtheisen.com>
2017-01-23 21:22:10 -0500

    Actually some semblance of working, also tracks function headers

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,21 @@
 import vim
 
-varList_C = ["int", "double", "char"]
+varKeys_C = ["int", "double", "char", "void"]
+varList = []
 
 vim.command("vnew")
+vim.command("wincmd r")
 cb = vim.current.buffer
 
 for line in vim.buffers[1]:
-    for var in varList_C:
+    for var in varKeys_C:
         if var in line:
-            cb.append(line)
+            lineList = line.split()
+            if lineList[0] in varKeys_C:
+                    varList.append(line)
+
+for var in varList:
+    if var not in cb:
+        cb.append(var)
+
+vim.command("wincmd w")
